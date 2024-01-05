965cd8f66d25acca6097ba66d0c99d36cfb1bb2b
Balázs Grill <balazs.grill@incquerylabs.com>
2024-01-05 18:23:08 +0100

    experimented with query & object creation

--- main.go
+++ main.go
@@ -8,16 +8,25 @@ import (
 
 	"github.com/anyproto/any-sync/app"
 	"github.com/anyproto/anytype-heart/core/application"
-	"github.com/anyproto/anytype-heart/core/block/collection"
+	"github.com/anyproto/anytype-heart/core/block"
+	"github.com/anyproto/anytype-heart/core/block/object/objectcreator"
+	"github.com/anyproto/anytype-heart/core/domain"
 	"github.com/anyproto/anytype-heart/core/event"
 	"github.com/anyproto/anytype-heart/pb"
+	"github.com/anyproto/anytype-heart/pkg/lib/bundle"
+	"github.com/anyproto/anytype-heart/pkg/lib/database"
+	"github.com/anyproto/anytype-heart/pkg/lib/localstore/objectstore"
+	"github.com/anyproto/anytype-heart/pkg/lib/pb/model"
 	"github.com/anyproto/anytype-heart/space"
+	"github.com/anyproto/anytype-heart/util/pbtypes"
+	"github.com/gogo/protobuf/types"
 )
 
 func main() {
 	path := flag.String("p", "", "storage path")
 	mnemonic := flag.String("m", "", "Secret mnemonic")
 	accountid := flag.String("a", "", "Account ID")
+	flag.Parse()
 
 	idle := &idledetector{
 		duration: time.Second,
@@ -46,8 +55,11 @@ func main() {
 	}
 	log.Println(token)
 	//ListenSessionEvents
-	//log.Println("Recovering account")
-	//err = instance.AccountRecover()
+	/*log.Println("Recovering account")
+	err = instance.AccountRecover()
+	if err != nil {
+		panic(err)
+	}*/
 
 	log.Println("AccountSelect")
 	_, err = instance.AccountSelect(context.Background(), &pb.RpcAccountSelectRequest{
@@ -65,8 +77,38 @@ func main() {
 	sp, _ := sps.GetPersonalSpace(context.Background())
 	log.Printf("Space: %s", sp.Id())
 
-	collections := app.MustComponent[*collection.Service](instance.GetApp())
-	log.Printf("%s: %s", collections.Name(), collections.CollectionType())
+	objectstores := app.MustComponent[objectstore.ObjectStore](instance.GetApp())
+
+	objectstores.Query(database.Query{
+		Filters: []*model.BlockContentDataviewFilter{
+			{
+				RelationKey: bundle.RelationKeySource.String(),
+				Condition:   model.BlockContentDataviewFilter_Equal,
+				Value:       pbtypes.String(""),
+			},
+		},
+	})
+
+	blockservice := app.MustComponent[*block.Service](instance.GetApp())
+	objectcreators := app.MustComponent[objectcreator.Service](instance.GetApp())
+	templateID := "mycustomobject"
+	smartblock, err := blockservice.GetObjectByFullID(context.Background(), domain.FullID{SpaceID: sp.Id(), ObjectID: templateID})
+	if err != nil {
+		createdobjectid, _, err := objectcreators.CreateObject(context.Background(), sp.Id(), objectcreator.CreateObjectRequest{
+			ObjectTypeKey: bundle.TypeKeyNote,
+			Details: &types.Struct{
+				Fields: map[string]*types.Value{
+					bundle.RelationKeyDescription.String(): pbtypes.String("Someting"),
+				},
+			},
+		})
+		if err != nil {
+			panic(err)
+		}
+		log.Printf("created ID: %s", createdobjectid)
+	} else {
+		log.Printf("ID: %s", smartblock.Id())
+	}
 
 	defer instance.Stop()
 }
